5ff2bffbba95c60ce7ef5ff43bf9aa59abdf028f
Maartje Eyskens <maartje@eyskens.me>
2020-01-09 18:55:27 +0100

    Add footer
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,14 @@
         <router-view/>
       </md-app-content>
     </md-app>
+    <footer>
+      <span>This site is built by Maartje Eyskens, data from NMBS/SNCB Train Map </span>
+      <a href="https://github.com/meyskens/where-is-my-train">Code on Github</a>
+    </footer>
   </div>
 </template>
+<style>
+  footer {
+    margin: 20px;
+  }
+</style>
